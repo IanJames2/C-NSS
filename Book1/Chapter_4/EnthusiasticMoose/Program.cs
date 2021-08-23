bff7d2080adb9df00474023e747fec8f93ee0254
IanJames2 <ianjms81@gmail.com>
2021-08-23 08:31:37 -0500

    Phase 4

--- Book1/Chapter_4/EnthusiasticMoose/Program.cs
+++ Book1/Chapter_4/EnthusiasticMoose/Program.cs
@@ -2,12 +2,16 @@
 
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
-Console.WriteLine();
 
 
-void MooseSays()
+// Let the moose speak!
+
+
+
+
+void MooseSays(string message)
 {
-    Console.WriteLine(@"
+    Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -24,7 +28,7 @@ void MooseSays()
                '-'    | /       \ |
                       |  .-. .-.  |
                       \ / o| |o \ /
-                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                       |   / \   |    {message}
                       / `^`   `^` \
                      /             \
                     | '._.'         \
@@ -37,4 +41,5 @@ void MooseSays()
     ");
 }
 
-MooseSays();
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
