50e141c5c64821deea2504a291844eff7c0c3bdf
IanJames2 <ianjms81@gmail.com>
2021-08-23 17:00:12 -0500

    translatorPartITBD

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,32 @@
-// Put your code here
+// // Put your code here
 
-const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
-];
+// const names = [
+//   "Nashville",
+//   "Hong Kong",
+//   "The back yard",
+//   "Earth",
+//   "London",
+//   "The mall",
+//   "Ryman Auditorium",
+//   "The Great Wall of China",
+// ];
 
-console.log("All Pace Names");
+// console.log("All Place Names");
 
-for (const name of names) {
-  console.log(name);
-}
+// for (const name of names) {
+//   console.log(name);
+// }
+
+const ChosenFew = () => {
+  const names = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China",
+  ];
+
+  console.log("All Place Names");
