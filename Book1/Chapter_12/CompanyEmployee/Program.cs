ba0f26d403246b8cd33917813e165377377b655f
IanJames2 <ianjms81@gmail.com>
2021-08-26 15:03:58 -0500

    The struggle is real

---
+++ Book1/Chapter_12/CompanyEmployee/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            var FedEx = new Company();
+
+            // Create three employees
+            var Frank = new Employee();
+            var Fred = new Employee()
+            var Ed = new Employee()
+
+            // Assign the employees to the company
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+        }
+    }
+}
