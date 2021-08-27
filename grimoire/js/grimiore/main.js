f64262819b8c25273a60962635ff853b51f990c0
IanJames2 <ianjms81@gmail.com>
2021-08-27 12:02:53 -0500

    close only counts in horseshoes

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -2,59 +2,61 @@
 console.log("Do you believe in magic?");
 console.log("------------------------");
 
-let allSpells = [
-  {
-    Name: "Turn enemy into a newt",
-    isEvil: true,
-    EnergyReqired: 5.1,
-  },
-  {
-    Name: "Conjure some gold for a local charity",
-    isEvil: false,
-    EnergyReqired: 2.99,
-  },
-  {
-    Name: "Give a deaf person the ability to heal",
-    isEvil: false,
-    EnergyReqired: 12.2,
-  },
-  {
-    Name: "Make yourself emperor of the universe",
-    isEvil: true,
-    EnergyReqired: 100.0,
-  },
-  {
-    Name: "Convince your relatives your political views are correct",
-    isEvil: false,
-    EnergyReqired: 2921.5,
-  },
-];
+const database = {
+  allSpells: [
+    {
+      Name: "Turn enemy into a newt",
+      isEvil: true,
+      EnergyReqired: 5.1,
+    },
+    {
+      Name: "Conjure some gold for a local charity",
+      isEvil: false,
+      EnergyReqired: 2.99,
+    },
+    {
+      Name: "Give a deaf person the ability to heal",
+      isEvil: false,
+      EnergyReqired: 12.2,
+    },
+    {
+      Name: "Make yourself emperor of the universe",
+      isEvil: true,
+      EnergyReqired: 100.0,
+    },
+    {
+      Name: "Convince your relatives your political views are correct",
+      isEvil: false,
+      EnergyReqired: 2921.5,
+    },
+  ],
+};
 
-const DisplaySpellBook = () => {
-  allSpells.map((allspell) => {
-    console.log(`{allSpell.Name}`);
+const getAllSpells = () => {
+  return database.allSpells.map((allSpells) => ({ ...allSpells }));
+};
+
+const DisplaySpellBookGood = () => {
+  console.log("Good Book");
+  const spellsx3 = getAllSpells();
+  spellsx3.map((allspell) => {
+    if (allspell.isEvil === true) {
+      console.log(` {allSpell.Name}`);
+    }
   });
 };
 
-DisplaySpellBook();
+DisplaySpellBookGood();
 
-// const SpellBook = () => {
+const DisplaySpellBookBad = () => {
+  console.log("   ");
+  console.log("Bad Book");
+  const spellsx2 = getAllSpells();
+  spellsx2.map((allspell) => {
+    if (allspell.isEvil === false) {
+      console.log(` {allSpell.Name}`);
+    }
+  });
+};
 
-//     {
-//         Title: "pass",
-//         let Spell = [
-
-//         ]
-//     }
-
-// }
-
-// const Spell = () => {
-//     [
-//         {
-//         Name: "pass",
-//         isEvil: false,
-//         EnergyReqired: 4.1
-//         }
-//     ]
-// }
+DisplaySpellBookBad();
