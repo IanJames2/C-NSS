0d784b9d364a2e0996f74a2490c7b454a88b729b
IanJames2 <ianjms81@gmail.com>
2021-09-01 11:49:21 -0500

    indented for loop syntax

--- Book1/Chapter_10/GuessingGame/Program.cs
+++ Book1/Chapter_10/GuessingGame/Program.cs
@@ -13,26 +13,26 @@ namespace GuessingGame
             var infinity =  int.MaxValue;
             for (var i = new int[] { 8, 6, 4, infinity }[difficulty - 1]; i > 0; i--)
             {
-            Console.WriteLine("Can you guess the secret number? Submit your guess below!");
-            Random random = new System.Random(); 
-            var secretNumber = random.Next(1, 100);
-            string response = Console.ReadLine();
-            if (int.Parse(response) == secretNumber) 
-            {
-                Console.WriteLine("Correct!");
-                Console.WriteLine($"{i} guesses remaining");
-                break;
-            }
-            else if (int.Parse(response) > secretNumber)
-            {
-                Console.WriteLine("Wrong! You're too high");
-                Console.WriteLine($"{i} guesses remaining");
-            }
-            else 
-            {
-                Console.WriteLine("Wrong! You're too low");
-                Console.WriteLine($"{i} guesses remaining");
-            }
+                Console.WriteLine("Can you guess the secret number? Submit your guess below!");
+                Random random = new System.Random(); 
+                var secretNumber = random.Next(1, 100);
+                string response = Console.ReadLine();
+                if (int.Parse(response) == secretNumber) 
+                {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine($"{i} guesses remaining");
+                    break;
+                }
+                else if (int.Parse(response) > secretNumber)
+                {
+                    Console.WriteLine("Wrong! You're too high");
+                    Console.WriteLine($"{i} guesses remaining");
+                }
+                else 
+                {
+                    Console.WriteLine("Wrong! You're too low");
+                    Console.WriteLine($"{i} guesses remaining");
+                }
             }
         }
     }
